721565c9bd403352d1ad23ac4c5ce38091169189
Tanveer88-88 <sitz_tani@hotmail.com>
2023-06-09 14:40:16 +0500

    vercel databas initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { client } from "@/lib/sanityClient";
 import { Image as IImage } from "sanity";
+import { urlForImage } from "../../sanity/lib/image";
 
 export const getProductData = async () => {
   const res = await client.fetch(`*[_type=="products"]{
@@ -29,9 +30,16 @@ interface IProduct {
 export default async function Home() {
   const data: IProduct[] = await getProductData();
   return (
-    <div>
+    <div className="grid grid-cols-[repeat(3,auto)] justify-center gap-x-10">
       {data.map((item) => (
-        <div className="">
+        <div>
+          <Image
+          width={200}
+          height={200} 
+          src={urlForImage(item.image).url()} alt="products"></Image>
+          <h2>{item.title}$</h2>
+          <p>{item.price}$</p>
+          <button className="border py-2 px-6 bg-blue-600 text-white rounded">Add to Cart</button>
         </div>
       ))}
     </div>
